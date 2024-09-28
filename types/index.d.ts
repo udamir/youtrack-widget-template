46b6175b5ac66e9e87443d7a3390e81a3620196c
Damir <damir@yusipov.ru>
2024-09-29 00:13:59 +0300

    fix types definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,8 @@ declare module "hub-dashboard-addons" {
   import type { DashboardApi, WidgetApi } from "youtrack-client"
 
   const addon: {
+    locale: string;
+    editable: boolean;
     registerWidget: (widget: (dashboardApi: DashboardApi, widgetApi: WidgetApi) => Promise<void>) => void
   }
 
